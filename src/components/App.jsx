0ecaf58f5deb36362c83b31aad198c7f3574d468
Mooncheetos <143450665+Mooncheetos@users.noreply.github.com>
2024-03-16 19:49:40 +0200

    FirCom

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import userData from "./Profile/userData.json";
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App
